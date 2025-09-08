76ba9edb8e38386a745e24df58c6718b164b63df
omardev <omar@die.ng>
2025-09-08 19:05:28 +0000

    Design3

--- src/app/services/meteoserv.ts
+++ src/app/services/meteoserv.ts
@@ -9,7 +9,12 @@ export class Meteoserv {
   apiKey = "aa3896d682001e8838ea641eccb8d758";
   apiUrl = "https://api.openweathermap.org/data/2.5/weather";
   constructor(private http: HttpClient) { }
+
   getWeather(city: string): Observable<string> {
     return this.http.get<string>(`${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric&lang=fr`);
+  }
+
+  getForecast(city: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/forecast?q=${city}&appid=${this.apiKey}&units=metric&lang=fr`);
   }
 }
